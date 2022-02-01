989f5aab1ee8b80ae8ce209be4eed0121d95f320
Python Impressionador <tjdeoliveira@outlook.com>
2022-02-01 11:02:56 -0300

    make it better

---
+++ comunidadeimpressionadora/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '46043ad58a1d8b0f07ea5b59baa9c927'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'alert-info'
+
+from comunidadeimpressionadora import routes
